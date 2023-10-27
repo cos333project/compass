6e92fce05e6bebd0a2f83cf63036c3db27718f81
Windsor Nguyen <mn4560@princeton.edu>
2023-10-26 21:17:42 -0400

    frontend patch

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5af6367cdbe4fa00"; }
+/******/ 		__webpack_require__.h = function() { return "e025f396273643ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,8 @@
-'use client'
+'use client';
+
 import { ApolloProvider } from '@apollo/client';
 import { Provider as ReduxProvider } from 'react-redux';
+import { useState, useEffect } from 'react';
 import store from '../store/store';
 import client from './apolloClient';
 import Navbar from '../components/Navbar';
@@ -8,6 +10,18 @@ import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 
 export default function Home() {
+  const [auth, setAuth] = useState<{ isAuthenticated: boolean; username: string | null }>({ isAuthenticated: false, username: null });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/is_authenticated') // Change once this goes to production
+      .then((res) => res.json())
+      .then((data) => setAuth({ isAuthenticated: data.authenticated, username: data.username }))
+      .catch((err) => console.error(`Auth check failed: ${err}`));
+  }, []);
+
+  if (loading) return null; // Need to make this a cool loader page while user waits for auth check
+
   return (
     <ApolloProvider client={client}>
       <ReduxProvider store={store}>
