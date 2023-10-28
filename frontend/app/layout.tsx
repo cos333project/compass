73901d37dce3d5c2e8c4ecba6d513b6ae9b031a3
Windsor Nguyen <mn4560@princeton.edu>
2023-10-28 01:31:30 -0400

    changed font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GeistSans, GeistMono } from 'geist/font'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Compass',
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body>{children}</body>
     </html>
   )
 }
