e7942e48ab9521e1e882129b3b50adf34fd129c0
Kaan Odabas <ko8584@princeton.edu>
2023-11-02 15:52:15 -0400

    basic sorting algorithm test

--- backend/compass/serializers.py
+++ backend/compass/serializers.py
@@ -1,10 +1,9 @@
-
 from rest_framework import serializers
 from .models import Course, Department
 
 class CourseSerializer(serializers.ModelSerializer):
-    code = serializers.CharField(source='department.code', read_only=True)
+    department_code = serializers.CharField(source='department.code', read_only=True)
 
     class Meta:
         model = Course
-        fields = ['code', 'catalog_number', 'title']
+        fields = ['department_code', 'catalog_number', 'title', 'distribution_area_short']
